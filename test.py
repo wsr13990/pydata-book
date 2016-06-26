4a988a319a2f1fc1dd0776528d406a41c8795546
WSR13990 <wahyusejatiroso@yahoo.co.id>
2016-06-26 22:12:10 +0800

    Page 24

--- test.py
+++ test.py
@@ -4,6 +4,7 @@ from collections import Counter
 from pandas import DataFrame, Series
 import pandas as pd
 import matplotlib.pyplot as plot
+import numpy as np
 
 path = 'ch02\\usagov_bitly_data2012-03-16-1331923249.txt'
 open(path).readline()
@@ -39,5 +40,14 @@ clean_tz = frame['tz'].fillna('Missing')
 clean_tz[clean_tz == ''] = 'Unknown'
 
 tz_counts = clean_tz.value_counts()
-tz_counts[:10].plot(kind = 'barh', rot = 0)
-plot.show()
+#tz_counts[:10].plot(kind = 'barh', rot = 0)
+#plot.show()
+results = Series([x.split()[0] for x in frame.a.dropna()])
+result = results.value_counts()[:8]
+#print(result)
+
+cframe = frame[frame.a.notnull()]
+operating_system = np.where(cframe['a'].str.contains('Windows')
+	,'Windows','Not Windows')
+print(operating_system[:5])
+by_tz_os = cframe.groupby(['tz', operating_system])
